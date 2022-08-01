5868d560df4feabe398dc03cb512dc07f58c71cd
Adel <adel.amirali@outlook.com>
2022-08-01 20:06:06 +0200

    Updates

---
+++ precache-manifest.25106f902752bc6cd256550b3be458d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6ce6af45ccf29ef9020",
+    "url": "/portfolio/static/css/main.75966f04.chunk.css"
+  },
+  {
+    "revision": "b6ce6af45ccf29ef9020",
+    "url": "/portfolio/static/js/main.b6ce6af4.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/portfolio/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "0284f2fa76fd70ed824a",
+    "url": "/portfolio/static/js/2.0284f2fa.chunk.js"
+  },
+  {
+    "revision": "4ee62a16aff34ec48acc",
+    "url": "/portfolio/static/js/runtime~main.4ee62a16.js"
+  },
+  {
+    "revision": "b815e18282241c217c15386ebc08692b",
+    "url": "/portfolio/static/media/myPhoto.b815e182.png"
+  },
+  {
+    "revision": "040a6cdc1c4615852243e00976c2da1f",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.470b1b541c8f099ee712c9dd0878f27f.js"
+  "/portfolio/precache-manifest.25106f902752bc6cd256550b3be458d9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
