06efecd08f67e15afa51670d33a5b08e88623989
Adel <adel.amirali@outlook.com>
2022-08-01 19:13:02 +0200

    Updates

---
+++ precache-manifest.470b1b541c8f099ee712c9dd0878f27f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "15d1f8941f377bb3a765",
+    "url": "/static/css/main.75966f04.chunk.css"
+  },
+  {
+    "revision": "15d1f8941f377bb3a765",
+    "url": "/static/js/main.15d1f894.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "0284f2fa76fd70ed824a",
+    "url": "/static/js/2.0284f2fa.chunk.js"
+  },
+  {
+    "revision": "36777d478a7e515f8b01",
+    "url": "/static/js/runtime~main.36777d47.js"
+  },
+  {
+    "revision": "b815e18282241c217c15386ebc08692b",
+    "url": "/static/media/myPhoto.b815e182.png"
+  },
+  {
+    "revision": "0a30ff0a4e1f1e70f23335abc139caad",
+    "url": "/index.html"
+  }
+];
